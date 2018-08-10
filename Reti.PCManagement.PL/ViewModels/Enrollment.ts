e34a061eb66ae6c3991f767e5ca36029a3d70be4
Marco <marco.comolli@reti.it>
2018-08-10 16:46:50 +0200

    Teacher and Enrollments FE classes

---
+++ Reti.PCManagement.PL/ViewModels/Enrollment.ts
@@ -0,0 +1,27 @@
+﻿import { Resource } from "./Resource";
+import { Course } from "./Course";
+
+export class Enrollment {
+    
+    public Id: number;
+    public Resource: Resource;
+    public ProjectLeader: Resource;
+    public Course: Course;
+    public StartDate: string | null;
+    public MaxEndDate: string | null;
+    public IsAdmitted: boolean;
+    public Notes: string;
+
+    constructor(id: number, res: Resource, leader: Resource, course: Course, startdate: string, endDate: string, admit: boolean, notes: string) {
+        this.Id = id;
+        this.Resource = res;
+        this.ProjectLeader = leader;
+        this.Course = course;
+        this.StartDate = startdate;
+        this.MaxEndDate = endDate;
+        this.IsAdmitted = admit;
+        this.Notes = notes;      
+    }
+
+
+}
---
+++ Reti.PCManagement.PL/ViewModels/Teacher.ts
@@ -0,0 +1,18 @@
+﻿import { Resource } from "./Resource";
+import { Course } from "./Course";
+
+export class Teacher {
+
+    public Id: number;
+    public Resource: Resource;
+    public Course: Course;
+    public Notes: string;
+
+    constructor(id: number, res: Resource, course: Course, notes: string) {
+        this.Id = id;
+        this.Resource = res;
+        this.Course = course;
+        this.Notes = notes;    
+    }
+
+}
