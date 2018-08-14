4580727862ec221ca0a41627d36a8e8e41cc9599
Marco <marco.comolli@reti.it>
2018-08-14 19:05:47 +0200

    Code cleaning

--- Reti.PCManagement.SL/Global.asax.cs
+++ Reti.PCManagement.SL/Global.asax.cs
@@ -14,18 +14,5 @@ namespace Reti.PCManagement.SL
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
 
-
-        //protected void Application_BeginRequest(Object sender, EventArgs e)
-        //{
-
-        //    if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
-        //        {
-        //            HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
-        //            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        //            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
-        //            HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
-        //            HttpContext.Current.Response.End();
-        //        }
-        //}
     }
 }
